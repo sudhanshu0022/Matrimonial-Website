78e1a74f60d13221a791a8faed507b0eff0baf04
sudhanshu0022 <agarwalsudhanshu772@gmail.com>
2025-08-03 00:00:11 +0530

    errorsolve

--- first/settings.py
+++ first/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-or9r=y2bc64$qdazulp^87@w&jjx_%_8%ds$@e8#&78=uix+5!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['matrimonial-website.onrender.com', 'localhost', '127.0.0.1']
 
@@ -134,3 +134,7 @@ LOGIN_URL = 'login'
 # Media files
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://matrimonial-website.onrender.com",
+]
